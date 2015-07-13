d192441a85b35578d38955e618133c71e1f08fca
NicoHood <NicoHood@users.noreply.github.com>
2015-07-13 20:09:55 +0200

    Example Updates

--- examples/MSGEQ7_Serial/MSGEQ7_Serial.ino
+++ examples/MSGEQ7_Serial/MSGEQ7_Serial.ino
@@ -1,14 +1,14 @@
 /*
- Copyright (c) 2014-2015 NicoHood
- See the readme for credit to other people.
+  Copyright (c) 2014-2015 NicoHood
+  See the readme for credit to other people.
 
- MSGEQ7 Serial example
- Output via Serial
+  MSGEQ7 Serial example
+  Output via Serial
 
- Needs no special hardware and displays raw values on the Serial monitor.
+  Needs no special hardware and displays raw values on the Serial monitor.
 
- Reads MSGEQ7 IC with 7 different frequencys from range 0-255
- 63Hz, 160Hz, 400Hz, 1kHz, 2.5kHz, 6.25KHz, 16kHz
+  Reads MSGEQ7 IC with 7 different frequencys from range 0-255
+  63Hz, 160Hz, 400Hz, 1kHz, 2.5kHz, 6.25KHz, 16kHz
 */
 
 // MSGEQ7
@@ -23,62 +23,93 @@
 CMSGEQ7<MSGEQ7_SMOOTH, pinReset, pinStrobe, pinAnalogLeft, pinAnalogRight> MSGEQ7;
 
 void setup() {
-  // this will set the IC ready for reading
+  // This will set the IC ready for reading
   MSGEQ7.begin();
 
   // Serial setup with faster baud rate gives more FPS (115200)
-  Serial.begin(9600);
-  Serial.println("Startup");
+  Serial.begin(115200);
+  Serial.println(F("Startup"));
 }
 
 void loop() {
-  // analyze without delay every interval
+  // Analyze without delay every interval
   bool newReading = MSGEQ7.read(MSGEQ7_INTERVAL);
 
-  // calculate FPS
+  // Calculate FPS
   uint16_t FPS = getFPS(newReading);
 
   // Serial raw debug output
-  if (newReading) {
-    // all channels together
-    Serial.print(MSGEQ7.get(MSGEQ7_BASS));
-    Serial.print(" ");
+  if (newReading)
+    serialBars(FPS);
+}
 
-    // 1st channel (here: left)
-    Serial.print(MSGEQ7.get(MSGEQ7_BASS, 0));
-    Serial.print(" ");
+void serialBars(uint16_t FPS) {
+  // Visualize the average bass of both channels
+  uint8_t input = MSGEQ7.get(MSGEQ7_BASS);
 
-    // 2nd channel (here: left)
-    Serial.print(MSGEQ7.get(MSGEQ7_BASS, 1));
-    Serial.print(" ");
+  // Reduce noise
+  input = MSGEQ7.map(input);
 
-    // overall volume of all channels
-    Serial.print(MSGEQ7.getVolume());
-    Serial.print(" ");
+  // Save the difference between the last beat
+  static uint8_t lastInput = 0;
+  int delta = input - lastInput;
+  lastInput = input;
 
-    // FPS of the reading
-    Serial.print(FPS);
-    Serial.print(" ");
+  // All channels together
+  Serial.print(input);
+  Serial.print(F(" In\t"));
 
-    // visualize with characters as bars
-    uint8_t input = MSGEQ7.get(MSGEQ7_BASS);
-    for (uint8_t i = 0; i < (input / 4); i++)
-      Serial.print('=');
-    Serial.println();
+  // Difference between last measurement
+  Serial.print(delta);
+  Serial.print(F(" D\t"));
+
+  // 1st channel (here: left)
+  Serial.print(MSGEQ7.get(MSGEQ7_BASS, 0));
+  Serial.print(F(" R\t"));
+
+  // 2nd channel (here: left)
+  Serial.print(MSGEQ7.get(MSGEQ7_BASS, 1));
+  Serial.print(F(" L\t"));
+
+  // overall volume of all channels
+  Serial.print(MSGEQ7.getVolume());
+  Serial.print(F(" Vol\t"));
+
+  // FPS of the reading
+  Serial.print(FPS);
+  Serial.print(F(" FPS \t"));
+
+  // Highlight high pitches
+  char c = '=';
+  if (delta >= 20)
+    c = '#';
+
+  // Visualize with characters as bars
+  for (uint8_t i = 0; i < (input / 4); i++)
+  {
+    if (i == 192 / 4 - 1)
+      Serial.print('+');
+    else if (i == 128 / 4 - 1)
+      Serial.print('*');
+    else if (i == 64 / 4 - 1)
+      Serial.print('X');
+    else
+      Serial.print(c);
   }
+  Serial.println();
 }
 
 uint16_t getFPS(bool newReading) {
-  // variables to count FPS and last 1 second mark
+  // Variables to count FPS and last 1 second mark
   static uint16_t prevFPS = 0;
   static uint16_t FPS = 0;
   static uint32_t m = 0;
 
-  // increase FPS count
+  // Increase FPS count
   if (newReading)
     FPS++;
 
-  // if 1 second mark crossed, save new FPS
+  // If 1 second mark crossed, save new FPS
   if ((micros() - m) > 1000000) {
     prevFPS = FPS;
     FPS = 0;
