98422aee5a813c2c97ac4deb2803adf043d5a86c
NicoHood <NicoHood@users.noreply.github.com>
2016-04-16 17:05:17 +0200

    Fix mapNoise() in FastLed example

--- examples/MSGEQ7_FastLED/MSGEQ7_FastLED.ino
+++ examples/MSGEQ7_FastLED/MSGEQ7_FastLED.ino
@@ -1,12 +1,12 @@
 /*
- Copyright (c) 2014-2015 NicoHood
- See the readme for credit to other people.
+  Copyright (c) 2014-2016 NicoHood
+  See the readme for credit to other people.
 
- MSGEQ7 FastLED example
- Output via Led strip and FastLED library
+  MSGEQ7 FastLED example
+  Output via Led strip and FastLED library
 
- Reads MSGEQ7 IC with 7 different frequencies from range 0-255
- 63Hz, 160Hz, 400Hz, 1kHz, 2.5kHz, 6.25KHz, 16kHz
+  Reads MSGEQ7 IC with 7 different frequencies from range 0-255
+  63Hz, 160Hz, 400Hz, 1kHz, 2.5kHz, 6.25KHz, 16kHz
 */
 
 // FastLED
@@ -18,7 +18,7 @@
 #define NUM_LEDS    32
 
 #define BRIGHTNESS  255  // reduce power consumption
-#define DITHER      255  // try 0 to disable flickering
+#define LED_DITHER  255  // try 0 to disable flickering
 #define CORRECTION  TypicalLEDStrip
 
 CRGB leds[NUM_LEDS]; // Define the array of leds
@@ -38,15 +38,15 @@ void setup() {
   // FastLED setup
   FastLED.addLeds<CHIPSET, LED_PINS, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(CORRECTION);
   FastLED.setBrightness( BRIGHTNESS );
-  FastLED.setDither(DITHER);
+  FastLED.setDither(LED_DITHER);
   FastLED.show(); // needed to reset leds to zero
 
-  // this will set the IC ready for reading
+  // This will set the IC ready for reading
   MSGEQ7.begin();
 }
 
 void loop() {
-  // analyze without delay
+  // Analyze without delay
   bool newReading = MSGEQ7.read(MSGEQ7_INTERVAL);
 
   // Led strip output
@@ -54,15 +54,15 @@ void loop() {
     // visualize the average bass of both channels
     uint8_t val = MSGEQ7.get(MSGEQ7_BASS);
 
-    // reduce noise
-    val = MSGEQ7.map(val);
+    // Reduce noise
+    val = mapNoise(val);
 
-    // visualize leds to the beat
+    // Visualize leds to the beat
     CRGB color = CRGB::Blue;
     color.nscale8_video(val);
     fill_solid(leds, NUM_LEDS, color);
 
-    // update Leds
+    // Update Leds
     FastLED.show();
   }
 }
